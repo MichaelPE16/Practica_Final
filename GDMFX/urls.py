1fdca1954da45e93da4dedadb5ca1d156e4bc0d1
michaelPE16 <rafael112216@gmail.com>
2025-10-30 02:04:01 -0400

    Adding a new id_caracter to details and forms pages

--- GDMFX/urls.py
+++ GDMFX/urls.py
@@ -13,8 +13,8 @@ urlpatterns =[
     path('meets', views.meets, name='meets'),
     path('reports', views.reports, name='reports'),
     path('sales', views.sales, name='sales'),
-    path('show_details', views.show_details, name='show_details'),
-    path('show_forms', views.show_forms, name='show_forms'),
+    path('show_details/<int:id_detalles>', views.show_details, name='show_details'),
+    path('show_forms/<int:id_forms>', views.show_forms, name='show_forms'),
     path('signup', views.signup_page, name='signup'),
 ]
 
