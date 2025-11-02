8d1e6d0a04931311d2a2835a5fe661289e0fc90d
michaelPE16 <rafael112216@gmail.com>
2025-11-01 20:21:51 -0400

    Adding login & sigup funtions and html,css styles, testuser, modifiying the staticFiles settings

--- GDMFX/views.py
+++ GDMFX/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+
 
 # Create your views here.
 def home(request): 
@@ -16,11 +21,34 @@ def inventory(request):
 def leads(request): 
     return render(request, 'leads.html')
 
-def login_page(request): 
-    return render(request, 'login.html')
+def login_page(request):
+    if request.method == "GET":
+        return render(request, 'login.html', {'form': AuthenticationForm})
+    else: 
+        log_username = request.POST['username']
+        log_password = request.POST['password']
+        user = authenticate(request, username = log_username, password = log_password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        return render(request, 'login.html', {'form': AuthenticationForm, 'error': 'User Or Password Incorrect'})
 
-def signup_page(request): 
-    return render(request, 'signup.html')
+
+
+
+def signup_page(request):
+    if request.method == 'GET': 
+        return render(request, 'signup.html', {'form': UserCreationForm})
+    else: 
+        if request.POST['password1'] == request.POST['password2']: 
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return redirect('login')
+            except IntegrityError:
+                return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'User already exist'})
+            
+    return render(request, 'signup.html', {'form': UserCreationForm, 'error': 'Passwords do not match'})
 
 def meets(request): 
     return render(request, 'meets.html')
@@ -31,8 +59,8 @@ def reports(request):
 def sales(request): 
     return render(request, 'sales.html')
 
-def show_details(request): 
+def show_details(request, id_details): 
     return render(request, 'show_details.html')
 
-def show_forms(request): 
+def show_forms(request, id_forms): 
     return render(request, 'show_forms.html')
