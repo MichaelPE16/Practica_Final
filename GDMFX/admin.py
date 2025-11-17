c3731086a917059ed654a5e593ea6cd1f7b1ca32
michaelPE16 <rafael112216@gmail.com>
2025-11-17 01:04:11 -0500

    Adding the admin registration tables

--- GDMFX/admin.py
+++ GDMFX/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Vehicles, Leads, Contract, Sales
 
-# Register your models here.
+
+class VehicleAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'vin', 'user')
+
+admin.site.register(Vehicles, VehicleAdmin)
+class LeadAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'name', 'user')
+
+admin.site.register(Leads, LeadAdmin)
+
+
+
+
