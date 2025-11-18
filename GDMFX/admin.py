b888a6fdf17e118cc9159cc3786e92309bad9b41
michaelPE16 <rafael112216@gmail.com>
2025-11-18 13:58:35 -0500

    Adding the table sales and contracts to admin.py

--- GDMFX/admin.py
+++ GDMFX/admin.py
@@ -6,11 +6,18 @@ class VehicleAdmin(admin.ModelAdmin):
     list_display = ('id', 'vin', 'user')
 
 admin.site.register(Vehicles, VehicleAdmin)
+
 class LeadAdmin(admin.ModelAdmin): 
     list_display = ('id', 'name', 'user')
 
 admin.site.register(Leads, LeadAdmin)
 
+class ContactAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'customer_name', 'user')
 
+admin.site.register(Contract, ContactAdmin)
 
+class SalesAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'lead', 'user')
 
+admin.site.register(Sales, SalesAdmin)
