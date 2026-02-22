589039be234d02bd4576211bc2c16e52a6a82b12
michaelPE16 <rafael112216@gmail.com>
2026-02-22 12:55:16 -0500

    Adding new api folder and installing restframework

---
+++ GDMFXapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GdmfxapiConfig(AppConfig):
+    name = 'GDMFXapi'
--- Gestor_DMFX/settings.py
+++ Gestor_DMFX/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GDMFX',
+    'GDMFXapi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
