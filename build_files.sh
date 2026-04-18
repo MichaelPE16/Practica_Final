278123db23ca2b22f211ab026b662057cf7eb973
michaelPE16 <rafael112216@gmail.com>
2026-04-18 12:58:22 -0400

    fix: re-adding pip bypass

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --noinput --clear
 python manage.py makemigrations
 python manage.py migrate
