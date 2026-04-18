36fa1a03800252ea5721e991328e8292db333bd4
michaelPE16 <rafael112216@gmail.com>
2026-04-18 13:01:10 -0400

    chore: suppress pip root warnings

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt --break-system-packages
+python3 -m pip install -r requirements.txt --break-system-packages --root-user-action=ignore
 python manage.py collectstatic --noinput --clear
 python manage.py makemigrations
 python manage.py migrate
