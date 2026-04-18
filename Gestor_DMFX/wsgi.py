c1dbdbffb5a924cca280ecf9016a8d710275eacb
michaelPE16 <rafael112216@gmail.com>
2026-04-18 12:17:57 -0400

    adding: Configure Vercel Deployment Settings

--- Gestor_DMFX/wsgi.py
+++ Gestor_DMFX/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Gestor_DMFX.settings')
 
 application = get_wsgi_application()
+
+# Alias designed for Vercel deployment compatibility
+app = application
